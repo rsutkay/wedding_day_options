cfc14a35f13ed55491059a97aea78443307df2f6
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:48:48 +0000

    Only drink_vote user can modify drink_vote

--- app/controllers/drink_votes_controller.rb
+++ app/controllers/drink_votes_controller.rb
@@ -1,4 +1,14 @@
 class DrinkVotesController < ApplicationController
+  before_action :current_user_must_be_drink_vote_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_drink_vote_user
+    drink_vote = DrinkVote.find(params[:id])
+
+    unless current_user == drink_vote.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @drink_votes = DrinkVote.all
 
