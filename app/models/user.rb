cac1511563ba007ead4b0d724090685735d3a3d9
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:47:41 +0000

    Direct association between users and buses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_one    :bus,
+             :dependent => :destroy
+
   has_many   :song_votes,
              :dependent => :destroy
 
