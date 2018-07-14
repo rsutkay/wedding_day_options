83b8a4f60da27c91942ed70b06209c83f1ad75a7
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:47:49 +0000

    Direct association between categories and drinks

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,9 @@
 class Drink < ApplicationRecord
   # Direct associations
 
+  belongs_to :category,
+             :counter_cache => true
+
   has_many   :drink_votes,
              :dependent => :destroy
 
