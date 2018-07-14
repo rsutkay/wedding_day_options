ccd6a1161bfa064844da6a3a0faa0816b834409f
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:48:36 +0000

    Whitelist columns in Song for ActiveAdmin

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Song do
+
+ permit_params :artist_id, :name, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
