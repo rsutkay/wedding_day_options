24176e81686df2f8ed443ddc7d93c9b0afb1b2b1
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:48:48 +0000

    Whitelist columns in Bus for ActiveAdmin

--- app/admin/buses.rb
+++ app/admin/buses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bus do
+
+ permit_params :need, :user_id, :passengers
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
