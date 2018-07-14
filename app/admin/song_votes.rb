30d82b668364baae726a41016deb455bcc1cd2fd
Raghu Betina <raghu@firstdraft.com>
2018-07-14 00:48:44 +0000

    Whitelist columns in SongVote for ActiveAdmin

--- app/admin/song_votes.rb
+++ app/admin/song_votes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register SongVote do
+
+ permit_params :user_id, :song_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
